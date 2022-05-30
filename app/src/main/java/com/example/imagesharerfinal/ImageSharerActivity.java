3d815db720b0462d71420c3bf1e3d8aaaff8eceb
cjkim <cjkim00@gmail.com>
2022-05-29 19:36:43 -0700

    Added methods that pick an image from the user's storage and uploads it to Google Firebase storage.

--- app/src/main/java/com/example/imagesharerfinal/ImageSharerActivity.java
+++ app/src/main/java/com/example/imagesharerfinal/ImageSharerActivity.java
@@ -1,12 +1,24 @@
 package com.example.imagesharerfinal;
 
+import android.app.Activity;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.util.Log;
 import android.view.View;
 import android.view.Menu;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.material.snackbar.Snackbar;
 import com.google.android.material.navigation.NavigationView;
 
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
+import androidx.annotation.NonNull;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
@@ -15,11 +27,21 @@ import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.imagesharerfinal.databinding.ActivityImageSharerBinding;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
+
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.IOException;
 
 public class ImageSharerActivity extends AppCompatActivity {
 
     private AppBarConfiguration mAppBarConfiguration;
     private ActivityImageSharerBinding binding;
+
+    FirebaseStorage firebaseStorage;
+    StorageReference storageReference;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,8 +54,7 @@ public class ImageSharerActivity extends AppCompatActivity {
         binding.appBarImageSharer.fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                openPhotoPicker();
             }
         });
         DrawerLayout drawer = binding.drawerLayout;
@@ -47,6 +68,10 @@ public class ImageSharerActivity extends AppCompatActivity {
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_image_sharer);
         NavigationUI.setupActionBarWithNavController(this, navController, mAppBarConfiguration);
         NavigationUI.setupWithNavController(navigationView, navController);
+
+        firebaseStorage = FirebaseStorage.getInstance();
+        storageReference = firebaseStorage.getReference();
+
     }
 
     @Override
@@ -62,4 +87,76 @@ public class ImageSharerActivity extends AppCompatActivity {
         return NavigationUI.navigateUp(navController, mAppBarConfiguration)
                 || super.onSupportNavigateUp();
     }
+
+    public void openPhotoPicker() {
+        // Launches photo picker in single-select mode.
+// This means that the user can select one photo or video.
+        Intent intent = new Intent();
+        intent.setType("image/*");
+        intent.setAction(Intent.ACTION_GET_CONTENT);
+
+
+
+
+        launchSomeActivity.launch(intent);
+
+    }
+
+    ActivityResultLauncher<Intent> launchSomeActivity
+            = registerForActivityResult(
+            new ActivityResultContracts
+                    .StartActivityForResult(),
+            result -> {
+                if (result.getResultCode()
+                        == Activity.RESULT_OK) {
+                    Intent data = result.getData();
+                    // do your operation from here....
+                    if (data != null
+                            && data.getData() != null) {
+                        Uri selectedImageUri = data.getData();
+                        //Bitmap selectedImageBitmap;
+                        try {
+                            Bitmap selectedImageBitmap
+                                    = MediaStore.Images.Media.getBitmap(
+                                    this.getContentResolver(),
+                                    selectedImageUri);
+
+                            Uri file = Uri.fromFile(new File(selectedImageUri.toString()));
+                            StorageReference imageReference = storageReference.child(selectedImageUri.toString());
+
+
+                            ByteArrayOutputStream baos = new ByteArrayOutputStream();
+                            selectedImageBitmap.compress(Bitmap.CompressFormat.JPEG, 100, baos);
+                            byte[] data2 = baos.toByteArray();
+                            UploadTask uploadTask = imageReference.putBytes(data2);
+                            uploadTask = imageReference.putFile(file);
+
+// Register observers to listen for when the download is done or if it fails
+                            uploadTask.addOnFailureListener(new OnFailureListener() {
+                                @Override
+                                public void onFailure(@NonNull Exception exception) {
+                                    // Handle unsuccessful uploads
+                                }
+                            }).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+                                @Override
+                                public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                                    // taskSnapshot.getMetadata() contains file metadata such as size, content-type, etc.
+                                    // ...
+                                }
+                            });
+
+                        }
+                        catch (IOException e) {
+                            e.printStackTrace();
+                        }
+                        Log.i("IMAGEURL", selectedImageUri.toString());
+
+
+
+
+
+                    }
+                }
+            });
+
 }
--- app/src/main/java/com/example/imagesharerfinal/MainActivity.java
+++ app/src/main/java/com/example/imagesharerfinal/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.imagesharerfinal;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.media.Image;
 import android.os.Bundle;
 
 import com.example.imagesharerfinal.Login.LoginActivity;
@@ -13,7 +14,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Intent intent = new Intent(this, LoginActivity.class);
+        //Intent intent = new Intent(this, LoginActivity.class);
+        Intent intent = new Intent(this, ImageSharerActivity.class);
         startActivity(intent);
     }
 }
